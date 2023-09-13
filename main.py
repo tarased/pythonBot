09435a70f7007e76ac192f0102e83f896ea4d9f0
vladislavicetkin <tarased@yandex.ru>
2023-09-13 14:49:46 +0300

    Добавлен постинг

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import telebot
 
+
+
 # токен бота, которого мы используем
 bot = telebot.TeleBot('6535944016:AAEEHR6N_pZeQQNRb-IKRI5vCqQoNmrBOFo')
 
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Привет! Я твой бот. Как я могу помочь тебе?")
+    bot.send_message(message.chat.id, "Привет! Я твой бот для постинга")
 
 # Обработчик текстовых сообщений
-@bot.message_handler(func=lambda message: True)
+'''@bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, message.text)
+    bot.reply_to(message, message.text)'''
+
+@bot.message_handler(commands=['post'])
+def handle_post(message):
+    channel_id = '-1001928782688'
+    text_to_post = 'YOUR_TEXT'
+    bot.send_message(channel_id, text_to_post)
+
 
 # Запуск бота
 if __name__ == "__main__":

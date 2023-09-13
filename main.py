95740d2d4784f5adea5092e30f7166c36c796cea
Владислав Ичеткин <vladislavicetkin@MacBook-Pro.local>
2023-09-13 14:31:12 +0300

    Первое изменение

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import telebot
 
-aaaa
+bot = telebot.TeleBot('6535944016:AAEEHR6N_pZeQQNRb-IKRI5vCqQoNmrBOFo')
+
+# Обработчик команды /start
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Привет! Я твой бот. Как я могу помочь тебе?")
+
+# Обработчик текстовых сообщений
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+# Запуск бота
+if __name__ == "__main__":
+    bot.polling()

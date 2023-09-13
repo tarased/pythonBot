2694fe85a7bdc8bfdcb046e06261aecfaf35c428
vladislavicetkin <tarased@yandex.ru>
2023-09-13 15:37:59 +0300

    Публикация в текущий момент / редактирование

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from telebot import types
 bot = telebot.TeleBot('6535944016:AAEEHR6N_pZeQQNRb-IKRI5vCqQoNmrBOFo')
 channel_id = '-1001928782688'
 user_state = {}
+publicText = ''
 
 
 # Обработчик команды /start
@@ -23,15 +24,30 @@ def handle_callback(call):
     if call.data == 'buttonCreatePost':
         bot.send_message(call.message.chat.id, "Какое сообщение вы хотите опубликовать?")
         user_state[call.message.chat.id] = "waiting_for_posttext"
+    if call.data == 'buttonPublic':
+        bot.send_message(channel_id, publicText)
+        bot.send_message(call.message.chat.id, f'Ваше сообщение {publicText} успешно опубликовано')
+        user_state[call.message.chat.id] = None
 
 
 # обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: user_state.get(message.chat.id) == "waiting_for_posttext")
 def handle_user_text(message):
+    global publicText
     user_input_text = message.text
-    bot.send_message(channel_id, user_input_text)
-    bot.send_message(message.chat.id, f'Ваше сообщение {user_input_text} успешно опубликовано')
+    # bot.send_message(channel_id, user_input_text)
+    markup = types.InlineKeyboardMarkup()
+    buttonPublic = types.InlineKeyboardButton('Опубликовать', callback_data='buttonPublic')
+    buttonAfter = types.InlineKeyboardButton('Отложить пост', callback_data='buttonAfter')
+    markup.add(buttonPublic, buttonAfter)
+    bot.send_message(message.chat.id, 'Вы желаете опубликовать пост прямо сейчас или отложить его?',
+                     reply_markup=markup)
+    publicText = user_input_text
     user_state[message.chat.id] = None
+
+
+
+
 
 
 @bot.message_handler(commands=['post'])
